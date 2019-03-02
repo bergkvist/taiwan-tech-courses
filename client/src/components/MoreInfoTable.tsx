b2dad5e94a2707d28e99b7fc2a35af49a528ae0d
Tobias Bergkvist <tobias@bergkv.ist>
2019-03-02 16:12:39 +0800

    Improve design. Search for description. Better display of course attributes

---
+++ client/src/components/MoreInfoTable.tsx
@@ -0,0 +1,43 @@
+import React, { Fragment } from 'react'
+import styled from 'styled-components'
+
+export interface MoreInfoRow { title: string, value: Element | string | number }
+
+const Table = styled.table`
+  width: 98%;
+  table-layout: fixed;
+  border: 1px solid lightgray;
+  border-collapse: collapse;
+  margin-right: 1%;
+  margin-left: 1%;
+`
+
+const Tr = styled.tr`
+  :nth-child(odd) {
+    background-color: #f2f7ff;
+  }
+  :nth-child(even) {
+    background-color: #f9fbff;
+  }
+`
+const Td = styled.td`
+  padding: 7px;
+  border: 1px solid lightgray;
+  font-size: 14px;
+  word-wrap:break-word;
+`
+
+const MoreInfoRow: React.SFC<MoreInfoRow> = props => <Fragment>
+  <Tr>
+    <Td style={{ width: '20%', textAlign: 'right' }}>{props.title}</Td>
+    <Td style={{ width: '80%' }}>{props.value}</Td>
+  </Tr>
+</Fragment>
+
+export const MoreInfoTable: React.SFC<{ rows: Array<MoreInfoRow> }> = props => <Fragment>
+  <Table><tbody>
+      {props.rows.map(row => 
+        <MoreInfoRow title={row.title} value={row.value} />
+      )}
+  </tbody></Table>
+</Fragment>
